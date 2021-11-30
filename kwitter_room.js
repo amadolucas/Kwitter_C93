e509c36a0a6bc1bc2c65eb77a7dd576b71817f1f
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-30 16:54:52 -0300

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -13,7 +13,7 @@ var firebaseConfig = {
 
 user_name = localStorage.getItem("user_name");
 
-document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+document.getElementById("user_name").innerHTML = "Bem-vindo(a) " + user_name + "!";
 
 function addRoom()
 {
